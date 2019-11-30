34c20258a0f1dd471e58aa15bbd07be64acf58a3
aludkiewicz <aludgm@gmail.com>
2019-11-30 12:50:26 +0100

    Added file reading utility & some test files/classes

---
+++ src/com/aoc/FileReader.java
@@ -0,0 +1,26 @@
+package com.aoc;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FileReader {
+
+    public List<String> read(String filename) {
+        URL resource = Thread.currentThread().getContextClassLoader().getResource("resources/hello.txt");
+
+        try {
+            return Files.readAllLines(Paths.get(resource.toURI()));
+        } catch (Exception e) {
+            System.out.println("Derp");
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+}
--- src/com/aoc/Hello.java
+++ src/com/aoc/Hello.java
@@ -1,8 +1,25 @@
 package com.aoc;
 
+import java.util.List;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FileReader reader = new FileReader();
+
+        List<String> lines = reader.read("hello.txt");
+
+        lines.forEach(line -> {
+
+            String[] lineParts = line.split(",");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < lineParts.length; i++) {
+                sb.append(lineParts[i]).append(" ");
+            }
+            sb.setLength(sb.length() - 1);
+
+            System.out.println(sb.toString());
+        });
     }
 }
