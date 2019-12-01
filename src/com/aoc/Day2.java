8734b489b515d9838a56f38b546b6526b9966526
aludkiewicz <aludgm@gmail.com>
2019-12-01 12:22:38 +0100

    Day 2 skeleton

---
+++ src/com/aoc/Day2.java
@@ -0,0 +1,14 @@
+package com.aoc;
+
+import java.util.List;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+
+        FileReader reader = new FileReader();
+        List<String> day2Input = reader.read("input_day_2");
+
+    }
+
+}
