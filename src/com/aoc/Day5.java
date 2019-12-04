e7c7a2194138e6ffdd2b14590365808d6678af07
aludkiewicz <aludgm@gmail.com>
2019-12-04 22:38:05 +0100

    Skeleton day 5

---
+++ src/com/aoc/Day5.java
@@ -0,0 +1,16 @@
+package com.aoc;
+
+import java.util.List;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+
+        FileReader reader = new FileReader();
+
+        List<String> day1Input = reader.read("input_day_5");
+
+    }
+
+
+}
