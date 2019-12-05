c1b3b06d8d862a84a056642fcaf7383c2066d6ce
aludkiewicz <aludgm@gmail.com>
2019-12-05 23:44:50 +0100

    Day 6&7 skeletons

---
+++ src/com/aoc/Day6.java
@@ -0,0 +1,14 @@
+package com.aoc;
+
+import java.util.List;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        FileReader reader = new FileReader();
+
+        List<String> day6Input = reader.read("input_day_6");
+
+    }
+}
