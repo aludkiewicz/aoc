bd5e842875f7c8a1d09f408dff788fad0bc3985e
aludkiewicz <aludgm@gmail.com>
2019-12-09 19:39:44 +0100

    Day 10 & 11 skeletons

---
+++ src/com/aoc/Day10.java
@@ -0,0 +1,14 @@
+package com.aoc;
+
+import java.util.List;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+
+        FileReader reader = new FileReader();
+        List<String> day10Input = reader.read("input_day_10");
+
+    }
+
+}
---
+++ src/com/aoc/Day11.java
@@ -0,0 +1,14 @@
+package com.aoc;
+
+import java.util.List;
+
+public class Day11 {
+
+    public static void main(String[] args) {
+
+        FileReader reader = new FileReader();
+        List<String> day11Input = reader.read("input_day_11");
+
+    }
+
+}
